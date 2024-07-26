52900f59310c1176292fb6832af2aeb2be94184d
Kavish Shah <kavishshah30@gmail.com>
2024-07-26 21:51:04 +0530

    meta tags added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Kavish | Portfolio</title>
-        <meta name="google-site-verification" content="NqX5mc07r18m3it3uZ5VvRW_mkz7NmFQ5r6ZZM8IRGk" />
+        <title>Kavish | Software Engineer | Web3 | Blockchain</title>
+        <meta
+          name="description"
+          content="Kavish Shah is a Software Engineer with hackathon wins like ETH India 2023 and Scaling Ethereum 2024. Specializes in Web3 and Blockchain technologies."
+        />
+        <meta
+          name="keywords"
+          content="Kavish, Shah, Software Engineer, Web3, Blockchain, ETH India, Scaling Ethereum, Portfolio, Eth Global"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="google-site-verification"
+          content="NqX5mc07r18m3it3uZ5VvRW_mkz7NmFQ5r6ZZM8IRGk"
+        />
       </Head>
       <body>
         <Main />
