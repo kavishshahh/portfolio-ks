9901c33f4909061ad3735b76dfc008da0896dd68
Kavish Shah <kavishshah30@gmail.com>
2024-07-26 09:26:41 +0530

    speed insights from vercel added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Layout from "../../components/Layout/Layout";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <Layout />
+      <SpeedInsights />
     </>
   );
 }
