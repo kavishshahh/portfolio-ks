2b9f0fb3e208f157987173f1051c0759d6854ab2
Kavish Shah <kavishshah30@gmail.com>
2025-03-14 20:26:00 +0530

    refactor Home component structure in index.tsx for improved readability and maintainability

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,15 @@ import Achievements from "../components/sections/Achievements/Achievements";
 import Experience from "../components/sections/Experience/Experience";
 import Hero from "../components/sections/Hero/Hero";
 
-      <main>
-        <Hero />
-        <About />
-        <Achievements />
-        <Experience />
-      </main> 
+const Home = () => {
+  return (
+    <main>
+      <Hero />
+      <About />
+      <Achievements />
+      <Experience />
+    </main>
+  );
+};
+
+export default Home; 
